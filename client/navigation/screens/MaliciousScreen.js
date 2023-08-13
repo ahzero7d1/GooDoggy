fb0d0a1e24cab87f5c9c34912bffecad24286492
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2023-08-14 04:56:59 +0900

    Home, Carbon 페이지 초안

--- client/navigation/screens/MaliciousScreen.js
+++ client/navigation/screens/MaliciousScreen.js
@@ -1,11 +1,25 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, Image, StyleSheet} from 'react-native';
 
 export default function MaliciousScreen({navigation}) {
     return(
-<View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+<View style={{flex: 1}}>
+    <View>
+    <Pressable onPress={() => navigation.navigate('홈')} style={({pressed}) => { return{opacity: pressed ? 0.5 : 1}}}>
+        <Image
+        style={styles.logoContainer}
+        source= {require('../../assets/pics/GooDoggy_logo.png')}
+        />
+    </Pressable>
+    <Pressable onPress={() => navigation.navigate('Account_Page')} style={({pressed}) => { return{opacity: pressed ? 0.5 : 1}}}>
+        <Image
+        style={styles.buttonContainer}
+        source= {require('../../assets/pics/profile_icon.png')}
+        />
+    </Pressable>
+    </View>
     <Text
-        onPress={() => navigation.navigate('Home')}
+        onPress={() => navigation.navigate('GooDoggy')}
         style={{fontSize: 26, fontWeight: 'bold'}}
     >
         Malicious Screen
@@ -13,3 +27,16 @@ export default function MaliciousScreen({navigation}) {
 </View>
     );
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        left: 360,
+        width: 30,
+        marginTop: 60
+    },
+    logoContainer: {
+        left: 25,
+        marginTop: 60,
+        position: 'absolute'
+    }
+})
