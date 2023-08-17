1dbb47e8c3d3da24139506cd37397b157c343c5f
haeun0228 <eileen0228@naver.com>
2023-08-17 20:03:08 +0900

    게시글 작성 페이지 수정

--- client/navigation/screens/BulletinWriteScreen.js
+++ client/navigation/screens/BulletinWriteScreen.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {View, Text, Pressable, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import { TextInput } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
+// import { useNavigation } from '@react-navigation/native';
 
 export default function BulletinWriteScreen({navigation, onCreate}) {
      //API 연결 
@@ -15,28 +15,35 @@ export default function BulletinWriteScreen({navigation, onCreate}) {
     //         return{
     //             id: dataID.current++,
     //             writer: it.writer,
-    //             title: it.title,
-    //             context: it.context,
-    //             select: false,
     //         };
     //     });
-    //     setData(initData);
-    // }
+    //     setState({
+    //      ...state,
+    //      writer: initData.writer});
+    //     }
 
     // React.useEffect(()=>{
     //     getData();
     // },[])
 
-    const [data, setData]=React.useState([]);
-    const [title, setTitle]=React.useState("");
-    const [context, setContext]=React.useState("");
+    // const [title, setTitle]=React.useState("");
+    // const [context, setContext]=React.useState("");
+
     const titleInput=React.useRef();
     const contextInput=React.useRef();
+    
+    const [title, setTitle]=React.useState("");
+    const [context, setContext] = React.useState("");
+    const [date, setDate]=React.useState(new Date());
  
     const [state, setState]=React.useState({
+        writer:"",
         title:"",
         context:"",
+        date: ""
     })
+
+    const writer = "장하은";
 
     const handleChangeState=(e)=>{
         setState({
@@ -47,15 +54,16 @@ export default function BulletinWriteScreen({navigation, onCreate}) {
 
     const handleSubmit=()=>{
         if(state.title.length<1 || state.title.length>30){
-            writerInput.current.focus();
+            titleInput.current.focus();
             return;
         }
         if(state.context.length<5||state.context.length>100){
             contextInput.current.focus();
             return;
         }
-        onCreate(state.title, state.context)
+        onCreate(writer,state.title, state.context)
         setState({
+            writer:"",
             title:"",
             context:"",
         })
@@ -77,12 +85,14 @@ export default function BulletinWriteScreen({navigation, onCreate}) {
                 </Pressable>
 
             <View style={styles.boxContainer}>
-                <TextInput ref={titleInput} name="title" style={styles.title} onPress={(e)=>setTitle(e.target.value)}
+                <TextInput ref={titleInput} value={title} style={styles.title} onPress={(e)=>setTitle(e.target.value)}
                     placeholder='제목 (30자 이내)'></TextInput>
                 <View style={styles.line}/>
-                <TextInput ref={contextInput} name="context" style={styles.context} onPress={(e)=>setContext(e.target.value)}
+                <TextInput ref={contextInput} value={context} style={styles.context} onPress={(e)=>setContext(e.target.value)}
                     placeholder='내용을 입력하세요.. (100자 이내)'></TextInput>
-                <Image onPress={()=>handleSubmit()} style={styles.doneButton} source={require('../../assets/pics/bulletinWrite_screen/doneButton.png')}></Image> 
+                <Pressable onPress={()=>handleSubmit()}>
+                    <Image style={styles.doneButton} source={require('../../assets/pics/bulletinWrite_screen/doneButton.png')}></Image> 
+                </Pressable>
             </View>
             <Pressable onPress={()=>navigation.goBack()}>
                 <Image style={styles.goBackButton} source={require('../../assets/pics/bulletinWrite_screen/goBackButton.png')}></Image> 
