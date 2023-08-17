87df54004a8d8f8f1fa86fc9e1b1c9776ab07f89
hyeinNa <nhi3373@gmail.com>
2023-08-17 21:44:52 +0900

    게시판 글쓰기 1차

--- server/goodoggy/goodoggy/forms.py
+++ server/goodoggy/goodoggy/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-
+from . import models
+from django.forms import ModelForm
 
 class FdbForm(forms.Form):
     good = forms.CharField(max_length=500)
     bad = forms.CharField(max_length=500)
+
+class BoardForm(ModelForm):
+    class Meta:
+        model = models.Board
+        fields = ['title', 'content', 'writer']
--- server/goodoggy/goodoggy/models.py
+++ server/goodoggy/goodoggy/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Feedback(models.Model):
     good = models.CharField(max_length=500)
     bad = models.CharField(max_length=500)
+
+class Board(models.Model):
+    title = models.CharField(max_length=20, null=True)
+    content = models.TextField()
+    writer = models.CharField(max_length=20, null=True)
